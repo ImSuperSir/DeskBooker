193746bf0dfb3cfe77923c17aa8fe20a1376df1e
ImsuperSir <ImsuperSir@outlook.com>
2022-11-19 16:38:57 -0800

    Agregando nuevos archivos

---
+++ DeskBooker.Core.Test/Processor/DeskBookingRequestProcessor.cs
@@ -0,0 +1,20 @@
+﻿namespace DeskBooker.Core.Processor
+{
+    internal class DeskBookingRequestProcessor
+    {
+        public DeskBookingRequestProcessor()
+        {
+        }
+
+        internal DeskBookingResult BookDesk(DeskBookingRequest request)
+        {
+            return new DeskBookingResult()
+            { 
+                FirstName = request.FirstName,
+                LastName = request.LastName,    
+                Email = request.Email,
+                Date = request.Date
+            };
+        }
+    }
+}
---
+++ DeskBooker.Core.Test/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,35 @@
+﻿namespace DeskBooker.Core.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+
+
+            //Arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Lauro",
+                LastName = "Ramirez",
+                Email = "imsupersir@outlook.com",
+                Date = new DateTime(2021, 1, 14)
+            };
+
+
+            
+            var processor = new DeskBookingRequestProcessor();
+
+            //Act
+            DeskBookingResult result =  processor.BookDesk(request);
+
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+
+        }
+    }
+}
